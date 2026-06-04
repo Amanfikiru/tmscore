d4091dc0d8d793de2aa9aae4e2b5f7d3b52206fe
Amanfikiru <Amanuelfikiru49@gmail.com>
2026-06-04 22:59:02 +0300

    Exercise 3: Pipeline Data Corruption

--- Program.cs
+++ Program.cs
@@ -13,11 +13,26 @@ var enrollment = new EnrollmentRecord(
 
 Console.WriteLine(enrollment);
 
-var course = new Course
-{
-    Code = "CS-401",
-    Title = "Advanced C#",
-    Capacity = 30
-};
-
+var course = new Course { Code = "CS-401", Title = "Advanced C#", Capacity = 30 };
 Console.WriteLine(course.Title);
+
+ 
+var s = new Student { Id = "S1", Name = "Abeba", Age = 20, GPA = 3.8m }; 
+Console.WriteLine($"Student: {s.Name}, GPA: {s.GPA}"); 
+Console.WriteLine();
+
+
+void PrintGradeReport(IEnumerable<IGradable> assessments) 
+{ 
+    Console.WriteLine("--- Grade Report ---"); 
+    foreach (var item in assessments) 
+    { 
+        Console.WriteLine($"{item.Title}: {item.CalculateGrade():F2}%"); 
+    } 
+} 
+IGradable[] cohortAssessments = [ 
+    new Quiz { Title = "C# Basics", CorrectAnswers = 18, TotalQuestions = 20 }, 
+    new LabAssignment { Title = "Registration API", FunctionalityScore = 90m, CodeQualityScore = 85m } 
+]; 
+ 
+PrintGradeReport(cohortAssessments); 
